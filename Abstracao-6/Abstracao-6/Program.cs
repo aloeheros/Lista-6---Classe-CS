93ac7b7a86f30fd818c67226fa7ef85ef3506ed3
Aloe <heros.dasilva@gmail.com>
2022-09-18 17:45:41 -0300

    Entrega lista classe

---
+++ Abstracao-6/Abstracao-6/Program.cs
@@ -0,0 +1,21 @@
+﻿using Abstracao_6.Src;
+using System;
+
+namespace Abstracao_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal gato = new Animal("Gato", 4, "Tem", "Não tem");
+            Animal cachorro = new Animal("Cachorro", 4, "Tem", "Não tem");
+            Animal peixe = new Animal("Peixe", 0, "Não tem.", "Não tem.");
+            Animal passarinho = new Animal("Pássaro", 2, "Não tem.", "Tem.");
+
+            Console.WriteLine(gato.ToString());
+            Console.WriteLine(cachorro.ToString());
+            Console.WriteLine(peixe.ToString());
+            Console.WriteLine(passarinho.ToString());
+        }
+    }
+}
---
+++ Abstracao-6/Abstracao-6/Src/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracao_6.Src
+{
+    public class Animal
+    {
+        string Nome { get; set; }
+
+        int Patas { get; set; }
+
+        string Pelos { get; set; }
+
+        string Bico { get; set; }
+
+        public Animal(string nome, int patas, string pelos, string bico)
+        {
+            Nome = nome;
+            Patas = patas;
+            Pelos = pelos;
+            Bico = bico;
+        }
+
+        public override string ToString()
+        {
+            return "O nome do animal é: " + Nome + "\n Quantidade de patas: " + Patas + "\n Tem pelos? " + Pelos + "\n Tem bico? " + Bico;
+            
+
+        }
+    }
+}
